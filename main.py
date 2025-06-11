cf35e1757e6cbe3ab45bade59b2be29e6b0fea5b
Arpon-programmer <arponb966@gmail.com>
2025-06-11 06:03:45 +0600

    Update execution count for a code cell in model design notebook

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
-from scripts import pc_parts_dataset
-fun = pc_parts_dataset.train_dataloader()
-print(fun)
+import torch
+import torchvision.transforms as transforms
+from PIL import Image
+
+model = torch.jit.load('Path of the model', map_location='cuda' if torch.cuda.is_available() else 'cpu')
+
+def preprocess_image(image_path, img_type="RGB"):
+    if img_type.loawer() == "rgb":
+        image = Image.open(image_path).convert("RGB")
+    elif img_type.lower() == "grayscale":
+        image = Image.open(image_path).convert("L")
+    else:
+        raise ValueError("Unsupported image type. Use 'RGB' or 'grayscale'.")
+    transform = transforms.Compose([
+        transforms.Resize((256, 256)),
+        transforms.ToTensor()
+    ])
+    return transform(image).unsqueeze(0)
